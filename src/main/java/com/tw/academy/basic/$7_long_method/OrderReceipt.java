29964e86db69e3d42124297d8a21dc4f1d1fdc07
theroadof <1294911641@qq.com>
2021-06-29 20:38:04 +0800

    delete method printCustomerName

--- src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
@@ -17,11 +17,6 @@ public class OrderReceipt {
         this.order = order;
     }
 
-    //Deprecated
-    public String printCustomerName() {
-        return order.getCustomerName();
-    }
-
     //todo: rename -- Tom
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
